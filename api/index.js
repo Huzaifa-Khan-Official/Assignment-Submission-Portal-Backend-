4b925b7aa8bc4dcfdf5d07f0b3dab3e8de56f22f
Huzaifa Khan <m.huzaifakhan2004@gmail.com>
2024-07-12 17:17:26 +0500

    change some cors related work

--- api/index.js
+++ api/index.js
@@ -1,3 +1,36 @@
+// import express from "express";
+// import cors from "cors";
+// import cookieParser from "cookie-parser";
+
+// // utiles
+// import connectDB from "../config/dbConfig.js";
+// import serverConfig from "../config/serverConfig.js";
+// import userRoutes from "../routes/userRoutes.js";
+// import assignmentRoutes from "../routes/assignmentRoutes.js";
+// import classRoutes from "../routes/classRoutes.js";
+
+// const port = serverConfig.PORT || 5000;
+
+// connectDB()
+//   .then((res) => console.log("successfully connected to database"))
+//   .catch((err) => console.log("Error connecting to database"));
+
+// const app = express();
+
+// app.use(express.json());
+// app.use(cors());
+
+// app.use(express.urlencoded({ extended: true }));
+
+// app.use(cookieParser());
+
+// app.use("/api/users", userRoutes);
+// app.use("/api/assignments", assignmentRoutes);
+// app.use("/api/classes", classRoutes);
+
+// app.listen(port, () => console.log(`Server listening on ${port}`));
+
+// export default app;
 import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
@@ -11,22 +44,49 @@ import classRoutes from "../routes/classRoutes.js";
 
 const port = serverConfig.PORT || 5000;
 
-connectDB()
-  .then((res) => console.log("successfully connected to database"))
-  .catch((err) => console.log("Error connecting to database"));
-
 const app = express();
 
+connectDB()
+  .then(() => console.log("Successfully connected to database"))
+  .catch((err) => {
+    console.error("Error connecting to database", err);
+    process.exit(1);  // Exit the process with an error code
+  });
+
+// List of allowed origins
+const allowedOrigins = [
+  'http://localhost:5173',
+  'https://hackathon-frontend-sandy.vercel.app'
+];
+
+// CORS configuration
+const corsOptions = {
+  origin: (origin, callback) => {
+    // Check if the request origin is in the allowed origins list
+    if (allowedOrigins.includes(origin) || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true,  // Allow credentials such as cookies
+};
+
+app.use(cors(corsOptions));
+
 app.use(express.json());
-app.use(cors());
-
 app.use(express.urlencoded({ extended: true }));
-
 app.use(cookieParser());
 
 app.use("/api/users", userRoutes);
 app.use("/api/assignments", assignmentRoutes);
 app.use("/api/classes", classRoutes);
+
+// Global error handler
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
 
 app.listen(port, () => console.log(`Server listening on ${port}`));
 

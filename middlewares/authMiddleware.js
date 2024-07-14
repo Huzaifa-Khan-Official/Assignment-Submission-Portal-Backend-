1c95c0c495c69e1f2b42df287200a8b2452a7dda
Abdulbasit110 <BASIT4408944@cloud.neduet.edu.pk>
2024-07-14 20:01:19 +0500

    changes in authenticate middleware

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -4,42 +4,46 @@ import asyncHandler from "../middlewares/asynHandler.js";
 import serverConfig from "../config/serverConfig.js";
 
 const authenticate = asyncHandler(async (req, res, next) => {
-    let token;
-
+  let token;
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer")
+  ) {
+    token = req.headers.authorization.split(" ")[1];
+  } else if (req.cookies.jwt) {
     token = req.cookies.jwt;
-    if (token) {
-        try {
+  }
+  //   token = req.cookies.jwt;
+  if (token) {
+    try {
+      const decoded = jwt.verify(token, serverConfig.jwtSecretKey);
 
-
-            const decoded = jwt.verify(token, serverConfig.jwtSecretKey);
-
-            req.user = await User.findById(decoded.userId).select("-password");
-            next();
-
-        } catch (error) {
-            res.status(401)
-            throw new Error("Not authorized, token failed");
-        }
-    } else {
-        res.status(401)
-        throw new Error("Not authorized, no token");
+      req.user = await User.findById(decoded.userId).select("-password");
+      next();
+    } catch (error) {
+      res.status(401);
+      throw new Error("Not authorized, token failed");
     }
-})
+  } else {
+    res.status(401);
+    throw new Error("Not authorized, no token");
+  }
+});
 
 const authorizeAdmin = (req, res, next) => {
-    if (req.user && req.user.role == "admin") {
-        next();
-    } else {
-        res.status(401).send("Not authorized as admin");
-    }
-}
+  if (req.user && req.user.role == "admin") {
+    next();
+  } else {
+    res.status(401).send("Not authorized as admin");
+  }
+};
 
 const authorizeTrainer = (req, res, next) => {
-    if (req.user && req.user.role == "trainer") {
-        next();
-    } else {
-        res.status(401).send("Not authorized!");
-    }
-}
+  if (req.user && req.user.role == "trainer") {
+    next();
+  } else {
+    res.status(401).send("Not authorized!");
+  }
+};
 
 export { authenticate, authorizeAdmin, authorizeTrainer };

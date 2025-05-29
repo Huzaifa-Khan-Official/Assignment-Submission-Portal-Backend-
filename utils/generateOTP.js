4b5f4c409e385364f9e4dd29185719f97af7f1f3
Huzaifa Khan <m.huzaifakhan2004@gmail.com>
2025-05-29 10:34:58 +0500

    updated generate otp code logic

--- utils/generateOTP.js
+++ utils/generateOTP.js
@@ -1,5 +1,5 @@
 const generateOtp = () => {
-    return Math.floor(1000 + Math.random() * 9999).toString()
+    return Math.floor(1000 + Math.random() * 9000).toString();
 }
 
 export { generateOtp }
